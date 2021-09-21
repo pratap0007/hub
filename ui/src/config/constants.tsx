3d6f9b561423a376f579163733b005911b2e84eb
pratap0007 <shverma@redhat.com>
2021-09-21 16:51:15 +0530

    Updates Auth UI to supports multiple auth options
    Signed-off-by: Shiv Verma <shverma@redhat.com>

--- ui/src/config/constants.tsx
+++ ui/src/config/constants.tsx
@@ -1,9 +1,12 @@
 window.config = window.config || {
   API_URL: 'no API_URL  set',
-  GH_CLIENT_ID: 'no GH_CLIENT_ID set'
+  GH_CLIENT_ID: 'no GH_CLIENT_ID set',
+  AUTH_BASE_URL: 'no AUTH_BASE_URL set'
 };
 
 export const API_URL = window.config.API_URL;
 export const GH_CLIENT_ID = window.config.GH_CLIENT_ID;
 export const API_VERSION = window.config.API_VERSION;
 export const GHE_URL = window.config.GHE_URL;
+export const AUTH_BASE_URL = window.config.AUTH_BASE_URL;
+export const REDIRECT_URI = window.config.REDIRECT_URI;
--- ui/src/containers/Header/Header.test.tsx
+++ ui/src/containers/Header/Header.test.tsx
@@ -4,7 +4,6 @@ import { when } from 'mobx';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from '.';
 import Search from '../../containers/Search';
-import UserProfile from '../UserProfile';
 import { FakeHub } from '../../api/testutil';
 import { createProviderAndStore } from '../../store/root';
 import { ActualDate, FakeDate } from '../../common/testutils';
@@ -57,34 +56,5 @@ describe('Header', () => {
 
     expect(component.find(Icon).length).toBe(1);
     expect(component.find(Icon).props().id).toBe(Icons.Help);
-  });
-
-  it('should render user profile', (done) => {
-    const component = mount(
-      <Provider>
-        <Router>
-          <Header />
-        </Router>
-      </Provider>
-    );
-    const { user } = root;
-    const code = {
-      code: 'foo'
-    };
-    user.authenticate(code);
-    user.updateRefreshToken();
-    when(
-      () => {
-        return !user.isLoading;
-      },
-      () => {
-        setTimeout(() => {
-          expect(user.isAuthenticated).toBe(true);
-          component.update();
-          expect(component.find(UserProfile).length).toEqual(1);
-          done();
-        }, 0);
-      }
-    );
   });
 });
